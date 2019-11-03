1c65fde77a89bd96a08b688b0b9f52de4713b7b9
reechee <richardokonicha@gmail.com>
2019-11-03 17:38:39 +0100

    app layout components

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,34 @@
 <template>
-  <v-app>
+  <v-app id="app">
     <v-app-bar
       color="blue darken-4"
       dark
       app
+      clipped-left
     >
-      <v-app-bar-nav-icon/>
+      <v-app-bar-nav-icon class="d-md-none" v-on:click="sideNav = !sideNav">
+      </v-app-bar-nav-icon>
       <v-toolbar-title>Dev Feast</v-toolbar-title>
+      <v-spacer/>
+      <v-toolbar-items >
+        <v-btn text class="d-none d-md-flex"> <v-icon left>mdi-call-split</v-icon>View Meetups</v-btn>
+      </v-toolbar-items>
     </v-app-bar>
 
-    <v-navigation-drawer v-model="sideNav" app color="blue darken-3" :mini-variant.sync="mini" permanent>
-      <v-list-item>
-        <v-list-item-avatar>
-          <v-img src=""></v-img>
-        </v-list-item-avatar>
-        <v-list-item-title>
-          John Leider
-        </v-list-item-title>
-      </v-list-item>
-      
+    <v-navigation-drawer clipped dark v-model="sideNav" app color="blue darken-3">
+      <v-list>
+        <v-subheader>Reports</v-subheader>
+        <v-list-item-group>
+          <v-list-item>
+            <v-list-item-icon>
+              <v-icon>mdi-call-split</v-icon>
+            </v-list-item-icon>
+            <v-list-item-content>
+              <v-list-item-title>Select a meetup</v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+        </v-list-item-group>
+      </v-list>
       
     </v-navigation-drawer>
     <v-content></v-content>
@@ -32,7 +42,19 @@ export default {
   },
   data(){
     return {
-      sideNav: true
+      sideNav: false,
+      menuItem: [
+        { icon: 'supervisor_account', title: 'View Meetups' },
+        { icon: 'room', title: 'Organize Meetup' },
+        { icon: 'person', title: 'Profile' },
+        { icon: 'face', title: 'Sign up' },
+        { icon: 'lock_open', title: 'Sign In' }
+      ]
+    }
+  },
+  methods: {
+    sideNavAction(){
+      this.sideNav = !this.sideNav
     }
   }
 };
