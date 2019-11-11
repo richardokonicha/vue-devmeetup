d2cbef03f88161f50c042073e47c5adafe571ecb
reechee <richardokonicha@gmail.com>
2019-11-11 15:00:59 +0100

    grid layout

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,11 +1,33 @@
 <template>
-  <v-container>
-    <v-row v-for="j in 100" :key="j" :class="j === 1 ? '' : 'mt-6'" no-gutters>
-      <v-col v-for="i in j + 1" :key="i">
-        <v-card class="pa-5" outlined>
-          {{ i }} of {{j + 1 }}
-        </v-card>
-      </v-col>
-    </v-row>
-  </v-container>
+    <v-container grey lighten-4>
+        <v-row v-for="j in 2" v-bind:key="j" no-gutters v-bind:class="j === 1 ? 'mb-5': ''">
+            <v-col v-for="i in j + 1" v-bind:key="i"  >
+                <v-card class="pa-5" tile outlined>
+                    {{ i }} of {{j + 1}}
+                </v-card>
+            </v-col>
+        </v-row>
+        <v-row class="mt-5" no-gutters>
+            <v-col v-for="i in 3" v-bind:key="i">
+                <v-card outlined tile class="pa-2">One of three columns</v-card>
+            </v-col>
+        </v-row>
+        <v-row class="my-5" no-gutters>
+            <template v-for="i in 4" >
+                <v-col :key="i">
+                    <v-card outlined tile class="pa-3">column</v-card>
+                </v-col>
+                <v-responsive v-if="i == 2" :key="`width-${i}`" width="100%"></v-responsive>
+            </template>
+        </v-row>
+
+        <v-row no-gutters class="my-5">
+            <v-col v-for="i in 4" :key="i">
+                <v-card outlined tile class="pa-3">
+                    column
+                </v-card>
+            </v-col>
+
+        </v-row>
+    </v-container>
 </template>
