7ce3f687bcdce7bba4512c37f71952fc327292d0
reechee <richardokonicha@gmail.com>
2019-11-12 08:26:27 +0100

    learn vuetify grid

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -27,7 +27,27 @@
                     column
                 </v-card>
             </v-col>
-
         </v-row>
+        <v-row class="mt-5" no-gutters>
+            <v-col v-for="i in 3" v-bind:key="i" :cols="i === 2 ? 6 : undefined">
+                <v-card outlined tile class="pa-2">
+                    {{i}} of 3 {{ i === 2 ? '(wider)': ''}}
+                </v-card>
+            </v-col>
+        </v-row>
+
+        <v-row no-gutters class="mt-5">
+            <v-col v-for='i in 3' :key="i"
+                   :md="i === 3 ? 8 : undefined"
+                   :sm="i === 1 ? 7 : undefined"
+                   :cols="i === 2 ? '8' : undefined"
+                    >
+                <v-card class="pa-2" outlined tile>
+                    {{i}} of 3
+                </v-card>
+            </v-col>
+        </v-row>
+
+
     </v-container>
 </template>
