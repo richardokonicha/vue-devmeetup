5c5b5d20524d8388e649354b61f98fed1e337f37
reechee <richardokonicha@gmail.com>
2019-11-15 10:45:41 +0100

    carousel and buttons

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,53 +1,36 @@
 <template>
-    <v-container grey lighten-4>
-        <v-row v-for="j in 2" v-bind:key="j" no-gutters v-bind:class="j === 1 ? 'mb-5': ''">
-            <v-col v-for="i in j + 1" v-bind:key="i"  >
-                <v-card class="pa-5" tile outlined>
-                    {{ i }} of {{j + 1}}
-                </v-card>
-            </v-col>
-        </v-row>
-        <v-row class="mt-5" no-gutters>
-            <v-col v-for="i in 3" v-bind:key="i">
-                <v-card outlined tile class="pa-2">One of three columns</v-card>
-            </v-col>
-        </v-row>
-        <v-row class="my-5" no-gutters>
-            <template v-for="i in 4" >
-                <v-col :key="i">
-                    <v-card outlined tile class="pa-3">column</v-card>
-                </v-col>
-                <v-responsive v-if="i == 2" :key="`width-${i}`" width="100%"></v-responsive>
-            </template>
-        </v-row>
+    <v-container>
 
-        <v-row no-gutters class="my-5">
-            <v-col v-for="i in 4" :key="i">
-                <v-card outlined tile class="pa-3">
-                    column
-                </v-card>
+        <v-row>
+            <v-carousel cycle interval="5000" vertical delimiter-icon="mdi-minus" :show-arrows="false" >
+                <v-carousel-item v-for="i in color" :key="i" transition="fade-transition" reverse-transition="fade-transition">
+                    <v-sheet elevation="5" class="" :color="i" height="100%">
+                        <v-row class="fill-height" justify="center" align="center">
+                            <div class="display-4">{{i}}</div>
+                        </v-row>
+                    </v-sheet>
+                </v-carousel-item>
+            </v-carousel>
+        </v-row>
+        <v-row justify="center">
+            <v-col :class="i === 2 ? 'blue': 'red'" v-for="i in 2" :key="i" >
+                <div :class="i === 1 ? 'text-right' : ''">
+                    <v-btn large outlined >meetup</v-btn>
+                </div>
             </v-col>
         </v-row>
-        <v-row class="mt-5" no-gutters>
-            <v-col v-for="i in 3" v-bind:key="i" :cols="i === 2 ? 6 : undefined">
-                <v-card outlined tile class="pa-2">
-                    {{i}} of 3 {{ i === 2 ? '(wider)': ''}}
-                </v-card>
-            </v-col>
-        </v-row>
-
-        <v-row no-gutters class="mt-5">
-            <v-col v-for='i in 3' :key="i"
-                   :md="i === 3 ? 8 : undefined"
-                   :sm="i === 1 ? 7 : undefined"
-                   :cols="i === 2 ? '8' : undefined"
-                    >
-                <v-card class="pa-2" outlined tile>
-                    {{i}} of 3
-                </v-card>
-            </v-col>
-        </v-row>
-
-
     </v-container>
 </template>
+<script>
+    export default {
+        name: 'home',
+        component: {
+    },
+        data(){
+            return {
+                color: ['green', 'blue', 'purple', 'orange']
+            }
+        }
+    }
+
+</script>
--- src/components/meetup/Meetups.vue
+++ src/components/meetup/Meetups.vue
@@ -1,13 +1,46 @@
 <template>
-<div>
-    meetups
-</div>
+    <div>
+<v-container  v-for="i in alignment" :key="i" :class="i.color" class="mb-6"  >
+    <v-row style="height: 150px" no-gutters :align="i.align" :order="i.order">
+        <v-col v-for="j in 3" :key="j" >
+            <v-card class="pa-5" tile outlined>One of three columns</v-card>
+        </v-col>
+    </v-row>
+</v-container>
+        <v-container>
+            <v-row>
+                <v-col order="2">
+                    <v-card class="pa-3">post</v-card>
+                </v-col>
+                <v-col order="5" offset="5">
+                    <v-card outlined tile class="pa-3">care</v-card>
+                </v-col>
+            </v-row>
+            <v-row>
+                <v-col offset="5">
+                    <v-card outlined tile class="pa-3 d-block" >
+                        wipe
+                    </v-card>
+                </v-col>
+            </v-row>
+        </v-container>
+    </div>
 </template>
 
 <script>
     export default {
-        name: "meetups"
+        name: "meetups",
+        data: () => ({
+            alignment : [
+                {color: 'red', align: 'start', order: 10},
+                {color: 'blue', align: 'center', order: 2},
+                {color: 'orange', align: 'end', order: 3},
+                {color: 'green', align: 'start', order: 4}
+            ],
+        })
+
     }
+;
 </script>
 
 <style scoped>
